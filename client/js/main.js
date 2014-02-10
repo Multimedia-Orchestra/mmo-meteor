8f8f46454415af534ce1495dc1375d3cdda944f3
Derek Razo <derekrazo@gmail.com>
2014-02-09 17:44:19 -0800

    first commit

---
+++ client/js/main.js
@@ -0,0 +1,13 @@
+Meteor.startup(function(){
+	Router.addRoute('/', 'homeTemplate');
+	Router.addRoute('/home', 'homeTemplate');
+	Router.addRoute('/about', 'aboutTemplate');
+	Router.addRoute('/press', 'pressTemplate');
+	Router.addRoute('/events', 'eventsTemplate');
+	Router.addRoute('/projects', 'projectsTemplate');
+	Router.addRoute('/people', 'projectsTemplate');
+	Router.addRoute('/chapters', 'chaptersTemplate');
+	Router.addRoute('/connect', 'connectTemplate');
+
+	Router.run();
+});
